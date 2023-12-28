1795dd3abc960b443674a43ddd5c1a799bbcd743
Vitaliy Semenov <synthex@bk.ru>
2023-12-28 09:10:16 +0600

    feat: add ElasticSearch engine

--- app-modules/search/src/Providers/SearchServiceProvider.php
+++ app-modules/search/src/Providers/SearchServiceProvider.php
@@ -3,14 +3,20 @@
 namespace Modules\Search\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Scout\EngineManager;
+use Modules\Search\Engine\ElasticSearchEngine;
 
 class SearchServiceProvider extends ServiceProvider
 {
-	public function register()
-	{
-	}
-	
-	public function boot()
-	{
-	}
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+        resolve(EngineManager::class)->extend('elasticsearch', function () {
+            return new ElasticSearchEngine();
+        });
+    }
 }
