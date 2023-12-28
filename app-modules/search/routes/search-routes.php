e80661749ca1e83e61ef6c1a3e9b7fcfe45e1b4e
Vitaliy Semenov <synthex@bk.ru>
2023-12-28 08:42:59 +0600

    chore: init search midule

---
+++ app-modules/search/routes/search-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Search\Http\Controllers\SearchController;
+
+// Route::get('/searches', [SearchController::class, 'index'])->name('searches.index');
+// Route::get('/searches/create', [SearchController::class, 'create'])->name('searches.create');
+// Route::post('/searches', [SearchController::class, 'store'])->name('searches.store');
+// Route::get('/searches/{search}', [SearchController::class, 'show'])->name('searches.show');
+// Route::get('/searches/{search}/edit', [SearchController::class, 'edit'])->name('searches.edit');
+// Route::put('/searches/{search}', [SearchController::class, 'update'])->name('searches.update');
+// Route::delete('/searches/{search}', [SearchController::class, 'destroy'])->name('searches.destroy');
---
+++ app-modules/search/src/Providers/SearchServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Search\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SearchServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
